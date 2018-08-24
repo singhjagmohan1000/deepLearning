560da422190578902ed0e75ca6fd6a6cb664bcb7
Jagmoah Singh <singhjagmohan4734@gmail.com>
2018-08-23 21:00:53 -0700

    completing the challenge

---
+++ IntrotoDeepLearning#1/challenge1.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+dataframe = pd.read_csv('challenge_dataset.txt')
+x_values = dataframe[[0]]
+
+
+y_values = dataframe[[1]]
+
+
+body_model = linear_model.LinearRegression()
+body_model.fit(x_values,y_values)
+
+print(body_model.coef_)
+
+print(np.mean((body_model.predict(x_values)-y_values)**2))
+
+print(body_model.score(x_values,y_values))
+
+
+plt.scatter(x_values,y_values)
+plt.plot(x_values,body_model.predict(x_values))
+
+plt.show()
